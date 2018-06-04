67f11d37bb69cb0e4a1c5bcc5003863c8812fd25
Vasko Kelkocev <pisimina@gmail.com>
2018-06-04 10:21:44 +0200

    limitless assets

--- app/assets.py
+++ app/assets.py
@@ -17,15 +17,20 @@ js = Bundle(
     'node_modules/jquery-pjax/jquery.pjax.js',
     'node_modules/bootbox/bootbox.js',
     'node_modules/bootstrap/dist/js/bootstrap.min.js',
+    'js/plugins/ui/nicescroll.min.js',
+    'js/plugins/ui/drilldown.js',
     'js/application.js',
     filters=(ConcatFilter, 'jsmin'),
     output='gen/packed.js'
 )
 
 css = Bundle(
-    'node_modules/bootstrap/dist/css/bootstrap.css',
-    'node_modules/font-awesome/css/font-awesome.css',
     'css/icons/icomoon/styles.css',
+    'node_modules/font-awesome/css/font-awesome.css',
+    'node_modules/bootstrap/dist/css/bootstrap.css',
+    'css/core.css',
+    'css/components.css',
+    'css/colors.css',
     'css/style.css',
     filters=('cssmin', 'cssrewrite'),
     output='gen/packed.css'
